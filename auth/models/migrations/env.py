0b4bc26bf830f4536928b851ac74ca188fd41ac0
Lubega Mark <lubegamark@gmail.com>
2018-04-15 12:07:12 +0300

    Add metadata to enable autogenration of migrations

--- auth/models/migrations/env.py
+++ auth/models/migrations/env.py
@@ -4,6 +4,8 @@ from alembic import context
 from sqlalchemy import create_engine, pool
 from logging.config import fileConfig
 
+from auth import models
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -16,7 +18,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.ModelBase.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

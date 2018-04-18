0b5a39ddb2e7d4ca1788722e194404914fa1701a
Lubega Mark <lubegamark@gmail.com>
2018-04-18 18:20:32 +0300

    Add flake8

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         'alembic==0.9.9',
         'bcrypt==3.1.4',
+        'flake8==3.5.0',
         'Flask==0.12.2',
         'Flask-Security==3.0.0',
         'Flask-SQLAlchemy==2.3.2',

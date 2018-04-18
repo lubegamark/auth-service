0ed78c2889ddf668a32219df541e1ce9aaf5a7f4
Lubega Mark <lubegamark@gmail.com>
2018-04-18 18:17:50 +0300

    Implement User registration

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, request
 import json
+import os
+
+from flask import Flask, request
+from flask_security import Security, SQLAlchemyUserDatastore
+from flask_security.utils import hash_password
+from flask_sqlalchemy import SQLAlchemy
 from voluptuous import (
     MultipleInvalid,
     Required,
     Schema)
 
+from auth.models import users, db_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super-secret'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SECURITY_PASSWORD_SALT"] = os.environ.get("SECURITY_PASSWORD_SALT") 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
+
+db = SQLAlchemy(app)
+db.session = db_session
+
+
+user_datastore = SQLAlchemyUserDatastore(
+    db,
+    users.User,
+    users.Role)
+security = Security(app, user_datastore)
+
 
 @app.route('/')
 def index():
@@ -28,13 +50,20 @@ def register():
         Required("phoneNumber"): str,
     })
 
-    print(request_data)
     try:
         validated = registration_schema(request_data)
+        validated["phone_number"] = validated.pop("phoneNumber")
+        validated["password"] = hash_password(validated["password"])
+        new_user = security.datastore.create_user(**validated)
+        db.session.commit()
     except MultipleInvalid as error:
         print(error)
-    return "OK"
+        return "Validation Error", 400
+    except Exception as error:
+        print(error)
+        return "Unknown Error occurred", 500
+    return "ok"
 
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)

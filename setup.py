42775be7d4701a85fce73ec18b7fa2b9159e6b8f
Lubega Mark <lubegamark@gmail.com>
2018-04-18 18:16:26 +0300

    Add bcrypt and Flask-SQLAlchemy to requirments

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ setup(
     include_package_data=True,
     install_requires=[
         'alembic==0.9.9',
+        'bcrypt==3.1.4',
         'Flask==0.12.2',
         'Flask-Security==3.0.0',
+        'Flask-SQLAlchemy==2.3.2',
         'psycopg2==2.7.4',
         'SQLAlchemy==1.2.6',
         'voluptuous==0.11.1'])

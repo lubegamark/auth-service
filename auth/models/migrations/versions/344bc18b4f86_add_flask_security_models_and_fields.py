353715356fdae1a131de11d3ac95fff7d0093b1c
Lubega Mark <lubegamark@gmail.com>
2018-04-18 13:03:26 +0300

    Add Roles Model and new fields to user

---
+++ auth/models/migrations/versions/344bc18b4f86_add_flask_security_models_and_fields.py
@@ -0,0 +1,70 @@
+"""Add Flask Security Models and fields
+
+Revision ID: 344bc18b4f86
+Revises: 1908608666b5
+Create Date: 2018-04-18 12:45:30.211169
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '344bc18b4f86'
+down_revision = '1908608666b5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'roles',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(length=80), nullable=True),
+        sa.Column('description', sa.String(length=255), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('name')
+    )
+    op.create_table(
+        'roles_users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('user_id', sa.Integer(), nullable=True),
+        sa.Column('role_id', sa.Integer(), nullable=True),
+        sa.ForeignKeyConstraint(['role_id'], ['roles.id'], ),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.add_column(
+        'users',
+        sa.Column('active', sa.Boolean(), nullable=True))
+    op.add_column(
+        'users', 
+        sa.Column('confirmed_at', sa.DateTime(), nullable=True))
+    op.add_column(
+        'users', 
+        sa.Column('current_login_at', sa.DateTime(), nullable=True))
+    op.add_column(
+        'users', 
+        sa.Column('current_login_ip', sa.String(length=100), nullable=True))
+    op.add_column(
+        'users',
+        sa.Column('last_login_at', sa.DateTime(), nullable=True))
+    op.add_column(
+        'users',
+        sa.Column('last_login_ip', sa.String(length=100), nullable=True))
+    op.add_column(
+        'users', 
+        sa.Column('login_count', sa.Integer(), nullable=True))
+
+
+def downgrade():
+    op.drop_column('users', 'login_count')
+    op.drop_column('users', 'last_login_ip')
+    op.drop_column('users', 'last_login_at')
+    op.drop_column('users', 'current_login_ip')
+    op.drop_column('users', 'current_login_at')
+    op.drop_column('users', 'confirmed_at')
+    op.drop_column('users', 'active')
+    op.drop_table('roles_users')
+    op.drop_table('roles')
+
--- auth/models/users.py
+++ auth/models/users.py
@@ -4,6 +4,26 @@ import sqlalchemy as sa
 from auth.models import ModelBase
 
 
+class RoleUser(ModelBase):
+    """Role and User intermediate Table
+    """
+    __tablename__ = 'roles_users'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'))
+    role_id = sa.Column(sa.Integer, sa.ForeignKey('roles.id'))
+
+
+class Role(ModelBase):
+    """Roles to handle permissions
+    """
+    __tablename__ = 'roles'
+
+    id = sa.Column(sa.Integer(), nullable=False, primary_key=True)
+    name = sa.Column(sa.String(80), unique=True)
+    description = sa.Column(sa.String(255))
+
+
 class User(ModelBase):
     """Users of the system
     """
@@ -16,4 +36,15 @@ class User(ModelBase):
     password = sa.Column(sa.String, nullable=False)
     email = sa.Column(sa.String, nullable=True)
     phone_number = sa.Column(sa.String, nullable=False)
-    
+    last_login_at = sa.Column(sa.DateTime())
+    current_login_at = sa.Column(sa.DateTime())
+    last_login_ip = sa.Column(sa.String(100))
+    current_login_ip = sa.Column(sa.String(100))
+    login_count = sa.Column(sa.Integer)
+    active = sa.Column(sa.Boolean())
+    confirmed_at = sa.Column(sa.DateTime())
+    roles = sa.orm.relationship(
+        'Role',
+        secondary='roles_users',
+        backref=sa.orm.backref('users', lazy='dynamic')
+    )

bcaeddd04182980d379061a77f5d67d7a81bc4cd
Lubega Mark <lubegamark@gmail.com>
2018-04-18 18:17:09 +0300

    Create database session from models

--- auth/models/__init__.py
+++ auth/models/__init__.py
@@ -1,4 +1,17 @@
+import os
+
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+database_uri = os.environ.get("DATABASE_URI")
+
+engine = create_engine(database_uri, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
 
 ModelBase = declarative_base()
+ModelBase.query = db_session.query_property()
+
 

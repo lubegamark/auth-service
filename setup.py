4f399b89b1ab66f681c897bbff22e2d571b6374a
Lubega Mark <lubegamark@gmail.com>
2018-04-18 13:02:26 +0300

    Add Flask Security to requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         'alembic==0.9.9',
         'Flask==0.12.2',
+        'Flask-Security==3.0.0',
         'psycopg2==2.7.4',
         'SQLAlchemy==1.2.6',
         'voluptuous==0.11.1'])

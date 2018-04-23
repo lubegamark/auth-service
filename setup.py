98b1e45db3a560ed13d67303629fcd6d232f3005
Lubega Mark <lubegamark@gmail.com>
2018-04-23 18:37:52 +0300

    Add extra information to setup.py and version

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
+import os
+
 from setuptools import setup
+
+
+NAME = 'gmoney-auth-service'
+DESCRIPTION = 'Gmoney Auth Service'
+URL = 'https://gitlab.com/g-money/authService'
+EMAIL = 'lubegamark@gmail.com'
+AUTHOR = 'Lubega Mark'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
 
 setup(
     name='gmoney-auth-service',
@@ -13,6 +33,6 @@ setup(
         'Flask-Security==3.0.0',
         'Flask-SQLAlchemy==2.3.2',
         'PyJWT==1.6.1',
-        'psycopg2==2.7.4',
+        'psycopg2-binary==2.7.4',
         'SQLAlchemy==1.2.6',
         'voluptuous==0.11.1'])

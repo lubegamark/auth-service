7611557ec2fcfd8856c75cb2cb400e87af3af5ea
Lubega Mark <lubegamark@gmail.com>
2018-04-26 20:07:28 +0300

    Use proper db_uri from environment

--- auth/models/__init__.py
+++ auth/models/__init__.py
@@ -5,21 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-db_provider = os.environ.get("DATABASE_PROVIDER")
-db_user = os.environ.get("DATABASE_USER")
-db_password = os.environ.get("DATABASE_PASSWORD")
-db_host = os.environ.get("DATABASE_HOST")
-db_port = os.environ.get("DATABASE_PORT")
-db_name = os.environ.get("DATABASE_NAME")
 
-database_uri = "{provider}://{user}:{password}@{host}:{port}/{db}".format(
-    provider=db_provider,
-    user=db_user,
-    password=db_password,
-    host=db_host,
-    port=db_port,
-    db=db_name
-)
+database_uri = os.environ.get('DATABASE_URI')
 
 engine = create_engine(database_uri, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
